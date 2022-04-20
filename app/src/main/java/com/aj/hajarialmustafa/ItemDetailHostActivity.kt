2d37603b91c2a6b183c6dc7ab153618170ca894c
ali johar <johar.ali1@gmail.com>
2022-04-20 20:10:18 +0430

    get data from json

--- app/src/main/java/com/aj/hajarialmustafa/ItemDetailHostActivity.kt
+++ app/src/main/java/com/aj/hajarialmustafa/ItemDetailHostActivity.kt
@@ -1,14 +1,30 @@
 package com.aj.hajarialmustafa
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.ServiceConnection
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.aj.hajarialmustafa.api.ServiceGenerator
 import com.aj.hajarialmustafa.databinding.ActivityItemDetailBinding
+import com.aj.hajarialmustafa.model.MakhtotItem
+import com.aj.hajarialmustafa.model.Post
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subscribers.DisposableSubscriber
 
 class ItemDetailHostActivity : AppCompatActivity() {
 
@@ -19,11 +35,52 @@ class ItemDetailHostActivity : AppCompatActivity() {
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL;
         val binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        connectionListener()
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_item_detail) as NavHostFragment
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
+    }
+
+    private fun connectionListener() {
+        val connectionManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectionManager.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                it.registerDefaultNetworkCallback(object: ConnectivityManager.NetworkCallback(){
+                    override fun onAvailable(network: Network) {
+                        checkOnlineJson()
+                    }
+                })
+            }
+        }
+    }
+
+    fun checkOnlineJson(){
+        if (true){
+            getMakhtotItemsOnline()
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun getMakhtotItemsOnline() {
+        ServiceGenerator.requestApi.getAllMakhtotItems()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object: DisposableSubscriber<MakhtotItem>(){
+                override fun onComplete() {
+                    Toast.makeText(this@ItemDetailHostActivity, getString(R.string.getData), Toast.LENGTH_LONG).show()
+                }
+
+                override fun onNext(t: MakhtotItem?) {
+                    val json: String = Gson().toJson(t?.posts)
+                    Log.i("AJC", json)
+                }
+
+                override fun onError(t: Throwable?) {
+                    Toast.makeText(this@ItemDetailHostActivity, getString(R.string.errorGettingData), Toast.LENGTH_LONG).show()
+                    Log.i("AJC", t?.message.toString())
+                }
+            })
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/aj/hajarialmustafa/api/RequestApi.kt
+++ app/src/main/java/com/aj/hajarialmustafa/api/RequestApi.kt
@@ -1,10 +1,11 @@
 package com.aj.hajarialmustafa.api
 
 import com.aj.hajarialmustafa.model.MakhtotItem
+import com.aj.hajarialmustafa.model.Post
 import io.reactivex.Flowable
 import retrofit2.http.GET
 
 interface RequestApi {
     @GET("posts")
-    fun getAllMakhtotItems(): Flowable<List<MakhtotItem>>
+    fun getAllMakhtotItems(): Flowable<MakhtotItem>
 }
